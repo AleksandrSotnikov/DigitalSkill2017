ae638f88ce359d57a56224c45b45899b47cf104b
Aleksandr Sotnikov <strelok11211@gmail.com>
2020-09-30 13:09:28 +0600

    Update

--- AuthCheck.xaml.cs
+++ AuthCheck.xaml.cs
@@ -32,7 +32,18 @@ namespace DigitalSkills2017
 
         private void dgView_LoadingRow(object sender, DataGridRowEventArgs e)
         {
+            if (((LoginUsers)e.Row.Item).Cause == "Soft") e.Row.Background = new SolidColorBrush(Colors.Red);
+            if (((LoginUsers)e.Row.Item).Cause == "System") e.Row.Background = new SolidColorBrush(Colors.Green);
+        }
 
+        private void Soft_Checked(object sender, RoutedEventArgs e)
+        {
+            dgView.ItemsSource = Manager.db.LoginUsers.Where(n => n.Cause == "Soft").ToList();
+        }
+
+        private void System_Checked(object sender, RoutedEventArgs e)
+        {
+            dgView.ItemsSource = Manager.db.LoginUsers.Where(n => n.Cause == "System").ToList();
         }
     }
 }
