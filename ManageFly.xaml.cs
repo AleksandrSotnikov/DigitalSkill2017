25d6310d2c25fd40cd30c21c5f3dfebf75859fd0
Aleksandr Sotnikov <strelok11211@gmail.com>
2020-10-05 14:07:00 +0600

    Update

--- ManageFly.xaml.cs
+++ ManageFly.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +24,18 @@ namespace DigitalSkills2017
         {
             InitializeComponent();
             Manager.db = new dbDigitalSkills2017Entities();
-            dgView.ItemsSource = Manager.db.Schedules.Select(n => new { n.Date, n.Time, Departure =  n.Routes.Airports.IATACode, Arrival = n.Routes.Airports1.IATACode,n.FlightNumber, n.Aircrafts.MakeModel,n.EconomyPrice,Business = (n.EconomyPrice*(Decimal)1.3), First = (n.EconomyPrice * (Decimal)1.5) }).ToList();
+            dgView.ItemsSource = Manager.db.Schedules.Select(n => new { n.Date, n.Time, Departure = n.Routes.Airports.IATACode, Arrival = n.Routes.Airports1.IATACode, n.FlightNumber, n.Aircrafts.MakeModel, n.EconomyPrice, Business = (n.EconomyPrice * (Decimal)1.3), First = (n.EconomyPrice * (Decimal)1.5) }).ToList();
+            cbFrom.ItemsSource = Manager.db.Airports.Select(n => n.IATACode).ToList();
+            cbTo.ItemsSource = Manager.db.Airports.Select(n => n.IATACode).ToList();
+            cbNumber.ItemsSource = Manager.db.Schedules.Select(n => n.FlightNumber).Distinct().ToList();
+            cbSort.ItemsSource = dgView.Columns.ToList().Select(n => n.Header).ToList();
+        }
 
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dgView.Columns[cbSort.SelectedIndex].SortDirection= ListSortDirection.Ascending;
+            //dgView.Items.Refresh();
+           // dgView_Sorting(dgView, new DataGridSortingEventArgs());
         }
     }
 }
